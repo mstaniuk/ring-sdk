13208a14ea46fc7da05b7c43f0f478e139f677c8
Mateusz Staniuk <mateuszst@kahoot.com>
2023-05-23 12:14:50 +0200

    feat: create simple Fetch API wrapper to handle json responses

---
+++ lib/core/fetcher.ts
@@ -0,0 +1,27 @@
+interface ClientOptions {
+  apiURL?: string;
+  apiKey?: string;
+}
+export const fetchData = async <T>(
+  url: string,
+  clientOptions: ClientOptions
+): Promise<T> => {
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${clientOptions.apiKey}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status} ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    // TODO: Handle error correctly
+    console.error('Error fetching data:', error);
+    throw new Error('Fetching data failed');
+  }
+};
